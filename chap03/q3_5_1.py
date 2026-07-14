11bf1d8891326d0cd9b7effc2c1d61e05061fd2a
itc-n26012 <n26012@std.it-college.ac.jp>
2026-07-14 01:18:26 +0000

    追加

--- chap03/q3_5_1.py
+++ chap03/q3_5_1.py
@@ -1,7 +1,8 @@
 a = 1
 while True:
     if a % 2 == 0:
-        print(a)
-        a += 1
-        if a >= 10:
-            break
+        print(a)  
+        
+    a += 1        
+    if a >= 10:   
+        break
