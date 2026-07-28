82c85ccf7d5420399434371cf9309c99cac72a0a
itc-n26012 <n26012@std.it-college.ac.jp>
2026-07-28 03:20:56 +0000

    Update q5_8_2.py

--- chap05/q5_8_2.py
+++ chap05/q5_8_2.py
@@ -1,17 +1,6 @@
-country_code = {
-        'Iceland': {'code': '354','capital': 'Reykjavik'},
-        'Ireland': {'code': '353', 'capital': 'Dublin'},
-        'Azerbaaidjan': {'code': '994', 'capital': 'Baku'}
-}
-
-def getstr_keyval(x):
-    if not isinstance(x, dict):
-        return x
-
-    my_str = ''
-    for key, val in x.items():
-        my_str += (' ' + str(key) + getstr_keyval(val))
-    return my_str
-
-for key1, val1 in country_code.items():
-    print(key1, getstr_keyval(val1))
+A = {x for x in range(21)}
+print(A)
+B = {x for x in range(21) if x % 2 == 0}
+print(B)
+C = A - B
+print(C)
